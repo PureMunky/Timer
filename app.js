bca6372f131a558bcfc6da56cef47bfca56f0820
Phil Corbett <corbett.phil@gmail.com>
2020-07-15 20:53:58 -0400

    Simple save function.

--- app.js
+++ app.js
@@ -1,69 +1,121 @@
 const Timer = (function () {
-    const currentName = document.getElementById('currentName');
-    const currentTime = document.getElementById('currentTime');
-    const txtTimer = document.getElementById('txtTimer');
-    const btnStart = document.getElementById('btnStart');
-    const btnStop = document.getElementById('btnStop');
+	const currentName = document.getElementById('currentName');
+	const currentTime = document.getElementById('currentTime');
+	const txtTitle = document.getElementById('txtTitle');
+	const txtTimer = document.getElementById('txtTimer');
+	const btnStart = document.getElementById('btnStart');
+	const btnStop = document.getElementById('btnStop');
+	const savedList = document.getElementById('savedList');
 
-    var running = false;
-    var started = null;
-    var currentTimers = [
-        {
-            Name: 'Hello',
-            Duration: 2000
-        },
-        {
-            Name: 'Goodbye',
-            Duration: 2000
-        }
-    ];
+	var running = false;
+	var started = null;
+	var currentTimers = [
+		{
+			Name: 'Hello',
+			Duration: 2000
+		},
+		{
+			Name: 'Goodbye',
+			Duration: 2000
+		}
+	];
+	var savedTimers = [
+		{
+			Title: 'Test',
+			Raw: 'Hello:2000|Goodbye:2000',
+			Timer: [
+				{
+					Name: 'Hello',
+					Duration: 2000
+				},
+				{
+					Name: 'Goodbye',
+					Duration: 2000
+				}        
+			]
+		}
+	]
 
-    function start() {
-        if(txtTimer.value.length > 0) {
-            currentTimers = txtTimer.value.split('|').map(x => {
-                return {
-                    Name: x.split(':')[0],
-                    Duration: x.split(':')[1]
-                };
-            });
-        }
+	function start() {
+		if(txtTimer.value.length > 0) {
+			currentTimers = convertStringToTimer(txtTimer.value);
+		}
 
-        running = true;
-        started = new Date();
-        tick();
-    }
+		running = true;
+		started = new Date();
+		tick();
+	}
 
-    function stop() {
-        running = false;
-    }
+	function convertStringToTimer(strInput) {
+		return strInput.split('|').map(x => {
+			return {
+				Name: x.split(':')[0],
+				Duration: x.split(':')[1]
+			};
+		});
+	}
 
-    function tick() {
-        let currentMS = new Date() - started;
-        let newMS = currentMS;
-        let found = false;
+	function stop() {
+		running = false;
+	}
 
-        for(var i = 0; i < currentTimers.length && !found; i++) {
-            if(newMS < currentTimers[i].Duration) {
-                found = true;
-                currentName.innerText = currentTimers[i].Name;
-            } else {
-                newMS = newMS - currentTimers[i].Duration;
-            }
-        }
+	function save() {
+		savedTimers.push({
+			Title: txtTitle.value,
+			Raw: txtTimer.value,
+			Timer: convertStringToTimer(txtTimer.value)
+		});
+		displaySavedTimers();
+	}
 
-        if(!found) {
-            currentName.innerText = 'Finished';
-            stop();
-        }
+	function tick() {
+		let currentMS = new Date() - started;
+		let newMS = currentMS;
+		let found = false;
 
-        currentTime.innerText = currentMS;
-        if(running) {
-            setTimeout(tick, 100);
-        }
-    }
+		for(var i = 0; i < currentTimers.length && !found; i++) {
+			if(newMS < currentTimers[i].Duration) {
+				found = true;
+				currentName.innerText = currentTimers[i].Name;
+			} else {
+				newMS = newMS - currentTimers[i].Duration;
+			}
+		}
 
-    return {
-        Start: start,
-        Stop: stop
-    };
+		if(!found) {
+			currentName.innerText = 'Finished';
+			stop();
+		}
+
+		currentTime.innerText = currentMS;
+		if(running) {
+			setTimeout(tick, 100);
+		}
+	}
+
+	function displaySavedTimers() {
+		var listHTML = '<ul>';
+
+		for(var i = 0; i < savedTimers.length; i++) {
+			listHTML += '<li><a href="#" onclick="Timer.LoadSaved(' + i + ')">' + savedTimers[i].Title + '</a></li>'
+		}
+
+		listHTML += '</ul>';
+
+		savedList.innerHTML = listHTML;
+	}
+
+	function loadSaved(i) {
+		txtTitle.value = savedTimers[i].Title;
+		txtTimer.value = savedTimers[i].Raw;
+	}
+
+	displaySavedTimers();
+
+	return {
+		Start: start,
+		Stop: stop,
+		Save: save,
+		LoadSaved: loadSaved
+	};
 }());
